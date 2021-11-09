27f3760d45aa62014bc8d4d134ed06d337bdaf31
terezadohnal <tddohnalova@gmail.com>
2021-11-09 19:53:55 +0100

    add login and logout

--- src/middleware.php
+++ src/middleware.php
@@ -1,5 +1,8 @@
 <?php
 // Application middleware
+//vydy kdyz prijde novy pozadavek, projde to pres tento skript
+
+//zde muzeme ukladat globalni promenne
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -7,5 +10,10 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->add(function(Request $request, Response $response, $next) {
     $basePath = $request->getUri()->getBasePath();
     $this->view->addParam('basePath', $basePath);
+    if (isset($_SESSION['logged_user'])) {
+        $this->view->addParam('loggedUser', $_SESSION['logged_user']);
+    }
     return $next($request, $response);
 });
+
+//endpoint je ulozen v $next a pres next se odkazeme na endpoint, ktery chceme navstivit
--- src/routes.php
+++ src/routes.php
@@ -44,95 +44,148 @@ $app->get('/users', function (Request $request, Response $response, $args) {
     return $this->view->render($response, 'users.latte', $data);
 })->setName('users');
 
+//seskupeni endpointu, ktere chceme zpristupnit pouze prihlasenym uzivatelum
+$app->group('/auth', function() use($app){
+    //do kazdeho endpointu automaticky doplni /auth
+
 // DELETE USER endpoint
 
-$app->get('/user/{id_person}/delete', function (Request $request, Response $response, $args) { //to id v {} znamena, ze se hodnota meni
-    // Render index view
-    try{
-        $stmt = $this->db->prepare('DELETE FROM person WHERE id_person = :idp');
-        $stmt->bindParam(':idp', $args['id_person']); //musi se shodovat v /users/{id_person}/..
+    $app->get('/user/{id_person}/delete', function (Request $request, Response $response, $args) { //to id v {} znamena, ze se hodnota meni
+        // Render index view
+        try{
+            $stmt = $this->db->prepare('DELETE FROM person WHERE id_person = :idp');
+            $stmt->bindParam(':idp', $args['id_person']); //musi se shodovat v /users/{id_person}/..
+            $stmt->execute();
+        }catch (Exeption $e){
+            echo var_dump($e);
+        }
+        return $response->withHeader('Location', $this->router->pathFor('users')); // tohle nas po vymazani presmeruje na endpoint users
+    })->setName('delete_user'); 
+
+    // LOCATION  location/{id_location}
+
+    $app->get('/location/{id_location}', function (Request $request, Response $response, $args) { //to id v {} znamena, ze se hodnota meni
+        // Render index view
+        try{
+            $stmt = $this->db->prepare('SELECT * FROM location WHERE id_location = :idl');
+            $stmt->bindParam(':idl', $args['id_location']); //musi se shodovat v /users/{id_person}/..
+            $stmt->execute();
+        }catch (Exeption $e){
+            echo var_dump($e);
+        }
+
+        $data = $stmt->fetch();
+        echo var_dump($data);
+
+        return $this->view->render($response, 'users.latte', $data);// tohle nas po vymazani presmeruje na endpoint users
+    })->setName('show_location');
+
+    //nacitani formulare new user
+    $app->get('/user/new', function (Request $request, Response $response, $args) {
+
+        $data['formData'] = [
+            'first_name' => '',
+            'last_name' => '',
+            'nickname' => '',
+            'gender' => '',
+            'height' => '',
+            'birth_day' => '',
+            'street_name' => '',
+            'street_number' => '',
+            'city' => '',
+            'zip' => ''
+        ];
+        return $this->view->render($response, 'user_new.latte', $data);
+    })->setName('user_new');
+
+    //zpracovani formulare
+    //metoda se podiva do tela pozadavku a sestavi nam z toho pole
+    $app->post('/user/new', function (Request $request, Response $response, $args){
+        $formData = $request->getParsedBody();
+
+        //zjisteni, jestli jsou prvni 3 atributy vyplnene
+        if(empty ($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname']) ) {
+            $data['message'] = 'Please fill required fields';
+        }else { // muzeme to zadat do databaze
+        //uzivatel vyplnil povinne inputy, zjistujeme adresu, zda vyplnil aspon jeden
+        if(!empty ($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['city']) || !empty($formData['zip']) ){
+            //uzivatel chce vytvorit novou adresu
+            $stmt = $this->db->prepare('INSERT INTO location (street_name, street_number, city, zip) VALUES (:sname, :snum, :city, :zip)');
+            //nevime, ktery parametr uziv vyplnil, musime vyplnit NULL, pokud
+            //pokud je pole nevyplnene, pouzijem podminku pomoci ternarniho operatoru
+            $stmt->bindParam(':sname', empty($formData['street_name']) ? null : $formData['street_name']);
+            $stmt->bindParam(':snum', empty($formData['street_number']) ? null : $formData['street_number']);
+            $stmt->bindParam(':city', empty($formData['city']) ? null : $formData['city']);
+            $stmt->bindParam(':zip', empty($formData['zip']) ? null : $formData['zip']);
+            $stmt->execute();
+            //chceme ziskat nove id lokace
+            $id_location = $this->db->lastInsertedId(); // vrati nam id noveho zaznamu
+        }
+
+        //do bindParams muzu hodit jen hotove hotdnoty, do bindValues i vyrazy a terenarni operatory
+        $stmt = $this->db->prepare('INSERT INTO person (nickname, first_name, last_name, birth_day, gender, height, id_location) VALUES (:nn, :fn, :ln, :bd, :gn, :hg, :idl)');
+
+        $stmt->bindValue(':nn', $formData['nickname']);
+        $stmt->bindValue(':fn', $formData['first_name']);
+        $stmt->bindValue(':ln', $formData['last_name']);
+        $stmt->bindValue(':bd', empty($formData['birth_day']) ? null : $formData['birth_day']);
+        $stmt->bindValue(':gn', empty($formData['gender']) ? null : $formData['gender']);
+        $stmt->bindValue(':hg', empty($formData['height']) ? null : $formData['height']);
+        $stmt->bindValue(':idl', $id_location ? $id_location : null);
         $stmt->execute();
-    }catch (Exeption $e){
-        echo var_dump($e);
+        $data['message'] = 'Person successfully inserted';
+        }
+        $data['formData'] = $formData;
+        return $this->view->render($response, 'user_new.latte', $data);
+    });
+
+    //Logout user
+    $app->get('/logout', function (Request $request, Response $response, $args) {
+        session_destroy(); //tohle staci
+        return $response->withHeader('Location', $this->router->pathFor('login'));
+        //presmerovani
+    })->setName('logout');
+
+})->add(function($request, $response, $next){
+    if(!empty($_SESSION['logged_user'])){ //overujem, jestli je uzivatel prihlaseny pomoci overeni existence session
+        return $next($request, $response);
+    }else {
+        return $response->withHeader('Location', $this->router->pathFor('login'));
+        //kdyz jsem odhlaseny, tak me to vykopne na prihlasovaci formular
     }
-    return $response->withHeader('Location', $this->router->pathFor('users')); // tohle nas po vymazani presmeruje na endpoint users
-})->setName('delete_user'); 
+});
 
-// LOCATION  location/{id_location}
 
-$app->get('/location/{id_location}', function (Request $request, Response $response, $args) { //to id v {} znamena, ze se hodnota meni
-    // Render index view
-    try{
-        $stmt = $this->db->prepare('SELECT * FROM location WHERE id_location = :idl');
-        $stmt->bindParam(':idl', $args['id_location']); //musi se shodovat v /users/{id_person}/..
-        $stmt->execute();
-    }catch (Exeption $e){
-        echo var_dump($e);
-    }
 
-    $data = $stmt->fetch();
-    echo var_dump($data);
+//render login form
+$app->get('/login', function (Request $request, Response $response, $args) {
+    return $this->view->render($response, 'login.latte');
+})->setName('login');
 
-    return $this->view->render($response, 'users.latte', $data);// tohle nas po vymazani presmeruje na endpoint users
-})->setName('show_location');
 
-//nacitani formulare new user
-$app->get('/user/new', function (Request $request, Response $response, $args) {
-
-    $data['formData'] = [
-        'first_name' => '',
-        'last_name' => '',
-        'nickname' => '',
-        'gender' => '',
-        'height' => '',
-        'birth_day' => '',
-        'street_name' => '',
-        'street_number' => '',
-        'city' => '',
-        'zip' => ''
-    ];
-    return $this->view->render($response, 'user_new.latte', $data);
-})->setName('user_new');
-
-//zpracovani formulare
-//metoda se podiva do tela pozadavku a sestavi nam z toho pole
-$app->post('/user/new', function (Request $request, Response $response, $args){
+//Autentizace usera
+$app->post('/login', function (Request $request, Response $response, $args) {
     $formData = $request->getParsedBody();
+    $passwd_hash = hash('sha256', $formData['password']);
 
-    //zjisteni, jestli jsou prvni 3 atributy vyplnene
-    if(empty ($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname']) ) {
-        $data['message'] = 'Please fill required fields';
-    }else { // muzeme to zadat do databaze
-       //uzivatel vyplnil povinne inputy, zjistujeme adresu, zda vyplnil aspon jeden
-       if(!empty ($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['city']) || !empty($formData['zip']) ){
-           //uzivatel chce vytvorit novou adresu
-           $stmt = $this->db->prepare('INSERT INTO location (street_name, street_number, city, zip) VALUES (:sname, :snum, :city, :zip)');
-           //nevime, ktery parametr uziv vyplnil, musime vyplnit NULL, pokud
-           //pokud je pole nevyplnene, pouzijem podminku pomoci ternarniho operatoru
-           $stmt->bindParam(':sname', empty($formData['street_name']) ? null : $formData['street_name']);
-           $stmt->bindParam(':snum', empty($formData['street_number']) ? null : $formData['street_number']);
-           $stmt->bindParam(':city', empty($formData['city']) ? null : $formData['city']);
-           $stmt->bindParam(':zip', empty($formData['zip']) ? null : $formData['zip']);
-           $stmt->execute();
-           //chceme ziskat nove id lokace
-           $id_location = $this->db->lastInsertedId(); // vrati nam id noveho zaznamu
-       }
+    $stmt = $this->db->prepare('SELECT id_person, first_name, nickname, last_name FROM person WHERE lower(nickname) = lower(:nn) AND password = :pswd'); // zjisteni, zda existuje uzivatel s takovym jmemen a heslem
+    // :nn a :pswd jsou vstupy od uzivatele
+    $stmt->bindValue(':nn', $formData['nickname']);
+    $stmt->bindValue(':pswd', $passwd_hash);
+    $stmt->execute();
 
-       //do bindParams muzu hodit jen hotove hotdnoty, do bindValues i vyrazy a terenarni operatory
-       $stmt = $this->db->prepare('INSERT INTO person (nickname, first_name, last_name, birth_day, gender, height, id_location) VALUES (:nn, :fn, :ln, :bd, :gn, :hg, :idl)');
+    $logged_user = $stmt->fetch(); //{'nicname'=> 'aaaa' ...}
 
-       $stmt->bindValue(':nn', $formData['nickname']);
-       $stmt->bindValue(':fn', $formData['first_name']);
-       $stmt->bindValue(':ln', $formData['last_name']);
-       $stmt->bindValue(':bd', empty($formData['birth_day']) ? null : $formData['birth_day']);
-       $stmt->bindValue(':gn', empty($formData['gender']) ? null : $formData['gender']);
-       $stmt->bindValue(':hg', empty($formData['height']) ? null : $formData['height']);
-       $stmt->bindValue(':idl', $id_location ? $id_location : null);
-       $stmt->execute();
-       $data['message'] = 'Person successfully inserted';
+    if($logged_user){
+        $_SESSION['logged_user'] = $logged_user;
+        setcookie('first_name', $logged_user['first_name']);
+        // je do nej ukladam vystup z databazoveho dotazu
+        // dulezity radek pro prihlaseni a autentizaci uivatele, tato promenna zije po dobu celeho 'sezeni'
+        return $response->withHeader('Location', $this->router->pathFor('users')); //nastavujem hlavicku a url na seznam mych uzivatelu
+    }else {
+        return $this->view->render($response, 'login.latte', ['message' => 'Wrong username']);
+        // v opacnem pripade ho posleme na prihlasovaci formular a posleme mu zpravu o chybnych udajich
     }
-    $data['formData'] = $formData;
-    return $this->view->render($response, 'user_new.latte', $data);
 });
 
 // in terminal:
